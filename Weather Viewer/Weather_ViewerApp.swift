16f3f74b1470d8886cb031213706ccbff5105978
Tiep Tran <t.anhtiep114@gmail.com>
2023-05-01 14:34:43 -0400

    Initial Commit

---
+++ Weather Viewer/Weather_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_ViewerApp.swift
+//  Weather Viewer
+//
+//  Created by Tiệp Trần on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Weather_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
